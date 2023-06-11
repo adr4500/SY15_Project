9b24f3359d1b75542a0e6f4bcc32d409b6cb44d4
mymanon2000 <102621797+mymanon2000@users.noreply.github.com>
2023-06-11 10:19:51 +0200

    Update state_estimation.py

--- workspace/src/SY15/scripts/state_estimation.py
+++ workspace/src/SY15/scripts/state_estimation.py
@@ -16,39 +16,39 @@ class StateEstimation:
         
         self.estimate_publisher = rospy.Publisher("estimation", PoseWithCovarianceStamped, queue_size=1)
 
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.predict)
         
         self.odom_subscriber = rospy.Subscriber("/odom", Odometry, self.receive_odom)
         self.imu_subscriber = rospy.Subscriber("/imu", Imu, self.receive_imu)
 
         # Matrices
-        self.P = np.eye(5) * 0.1 # Matrice de covariance de l'état
+        self.P = np.eye(5) * 0.00001 # Matrice de covariance de l'état
+        print("INIT")
+        print(self.P)
         self.X = np.zeros((5,1)) # Vecteur d'état
-        self.F = np.zeros((5,5)) # Matrice de transition d'état
+        self.F = np.eye(5) # Matrice de transition d'état
         self.C = np.zeros((2,5)) # Matrice de transition de mesure
-        self.Q = np.zeros((5,5)) # Matrice de covariance du bruit de processus
-        self.R = np.zeros((2,2)) # Matrice de covariance du bruit de mesure
+        self.Q = np.eye(5) * 0.000001 # Matrice de covariance du bruit de processus
+        self.R = np.eye(2) # Matrice de covariance du bruit de mesure
 
+        self.R[0,0] = 0.001
+        self.R[1,1] = 0.00001
         # Initialisation des matrices
         self.C[0,3] = 1
         self.C[1,4] = 1
-        self.F[0,0] = 1
-        self.F[1,1] = 1
-        self.F[2,2] = 1
 
         self.K = self.P @ self.C.T @ np.linalg.inv(self.C @ self.P @ self.C.T + self.R) # Matrice de gain de Kalman
 
-        self.last_time = 0
+        self.last_time = rospy.Time.now().to_sec()
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.predict)
         
-        
-    def predict(self):
+    def predict(self,event_timer):
 
         new_time = rospy.Time.now().to_sec()
 
         # Calcul de la matrice F
-        self.F[0,2] = -self.X[3] * math.sin(self.X[2]) * (new_time - self.last_time) * self.X[4]
+        self.F[0,2] = -self.X[3] * math.sin(self.X[2]) * (new_time - self.last_time)
         self.F[0,3] = math.cos(self.X[2]) * (new_time - self.last_time)
-        self.F[1,2] = self.X[3] * math.cos(self.X[2]) * (new_time - self.last_time) * self.X[4]
+        self.F[1,2] = self.X[3] * math.cos(self.X[2]) * (new_time - self.last_time)
         self.F[1,3] = math.sin(self.X[2]) * (new_time - self.last_time)
         self.F[2,4] = (new_time - self.last_time)
 
@@ -60,20 +60,24 @@ class StateEstimation:
         self.last_time = new_time
         
         # Calcul de la matrice P
+        
         self.P = self.F @ self.P @ self.F.T + self.Q
         
         self.publish_estimate()
         
         
     def receive_odom(self, odom_msg:Odometry):
+        
         linear_velocity = odom_msg.twist.twist.linear.x
         angular_velocity = odom_msg.twist.twist.angular.z
+        if abs(angular_velocity) > 2 :
+            return
         
         self.X = self.X + self.K @ (np.array([[linear_velocity], [angular_velocity]]) - self.C @ self.X)
         self.P = (np.eye(5) - self.K @ self.C) @ self.P
         self.K = self.P @ self.C.T @ np.linalg.inv(self.C @ self.P @ self.C.T + self.R)
         
-
+        
         
     def receive_imu(self, imu_msg:Imu):
         linear_acceleration = imu_msg.linear_acceleration # Attention, contient .x .y et .z
@@ -94,7 +98,7 @@ class StateEstimation:
         
         msg = PoseWithCovarianceStamped()
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = "base_footprint"
+        msg.header.frame_id = "odom"
         
         msg.pose.pose.position.x = x
         msg.pose.pose.position.y = y
