1a671c6fbc715d71740b7791e22ec1d31101217d
Adrien Bigotte <adr4500@gmail.com>
2023-06-22 12:41:15 +0200

    Second part of lab

--- workspace/src/SY15/scripts/labyrinth.py
+++ workspace/src/SY15/scripts/labyrinth.py
@@ -9,6 +9,8 @@ from enum import Enum
 # Enum of directions
 Direction = Enum('Direction',['UP','RIGHT','DOWN','LEFT'])
 
+State = Enum('State',['MOVING_TO_EDGE','WAITING','MOVING_TO_CENTER','MAPPING'])
+
 class Tile :
     '''This class represents one cell of the labyrinth, the labyrinth is defined recursively as a Tile tree'''
     def __init__(self,parent=None):
@@ -16,6 +18,7 @@ class Tile :
         self.parent = parent
         self.parent_direction = None
         self.children = [None,None,None,None] # [up,right,down,left]
+        self.visited = False
     
     def get_center(self):
         '''Returns the center of the cell'''
@@ -56,6 +59,37 @@ class Tile :
                 self.children[direction].set_corner(1,self.corners[0])
                 self.children[direction].set_corner(3,self.corners[2])
                 self.children[direction].parent_direction = Direction.RIGHT
+        
+    def extrapolate_corners(self):
+        # If two diagonal corners are defined, extrapolate orthogonaly the two others
+        if self.corners[0] is not None and self.corners[3] is not None :
+            if self.corners[1] is None :
+                self.corners[1][0] = self.corners[0][0]
+                self.corners[1][1] = self.corners[3][1]
+            if self.corners[2] is None :
+                self.corners[2][0] = self.corners[3][0]
+                self.corners[2][1] = self.corners[0][1]
+        elif self.corners[1] is not None and self.corners[2] is not None :
+            if self.corners[0] is None :
+                self.corners[0][0] = self.corners[1][0]
+                self.corners[0][1] = self.corners[2][1]
+            if self.corners[3] is None :
+                self.corners[3][0] = self.corners[2][0]
+                self.corners[3][1] = self.corners[1][1]
+    
+    def get_parent_edge(self):
+        '''Returns the entrance of the parent cell'''
+        if self.parent is None :
+            raise ValueError("The cell has no parent")
+        else :
+            if self.parent_direction == Direction.UP :
+                return np.mean([self.corners[2],self.corners[3]],axis=0)
+            elif self.parent_direction == Direction.RIGHT :
+                return np.mean([self.corners[0],self.corners[2]],axis=0)
+            elif self.parent_direction == Direction.DOWN :
+                return np.mean([self.corners[0],self.corners[1]],axis=0)
+            elif self.parent_direction == Direction.LEFT :
+                return np.mean([self.corners[1],self.corners[3]],axis=0)
 
     
 class Labyrinth_Solver:
@@ -65,10 +99,11 @@ class Labyrinth_Solver:
         
         self.first_tile = None
         self.current_tile = None
+        self.tile_size = None
 
         self.robot_pos = None
 
-        self.moving = False
+        self.state = None
         
         self.lidar_subscriber = rospy.Subscriber('/lidar/polylines',MarkerArray,self.lidar_callback)
         self.target_publisher = rospy.Publisher('/target',Pose,queue_size=10)
@@ -79,7 +114,7 @@ class Labyrinth_Solver:
     def lidar_callback(self,data):
         '''Callback function for the lidar'''
         # If the robot is moving, wait for it to stop
-        if self.moving or not self.robot_pos:
+        if self.state == State.MOVING_TO_EDGE or self.state == State.MOVING_TO_CENTER :
             return
         
         # Initialization of the first tile
@@ -115,19 +150,120 @@ class Labyrinth_Solver:
                 self.first_tile.set_corner(3,[entrance_points[1][0],entrance_points[1][1]])
                 self.first_tile.parent_direction = Direction.DOWN
 
+                # Initialize tile size from the entrance
+                self.tile_size = np.linalg.norm(entrance_points[0]-entrance_points[1])
+
                 # Go to entrance
                 target_coord = np.mean(entrance_points,axis=0)
                 target = Pose(Point(target_coord[0],target_coord[1],0),Quaternion())
                 print(target)
                 self.target_publisher.publish(target)
                 self.current_tile = self.first_tile
-                self.moving = True
+                self.state = State.MOVING_TO_EDGE
+                self.current_tile.visited = True
                 return
         
         else :
-            # Map the tile the robot just entered
-            pass
-            
+            if self.state == State.WAITING :
+                # Bring the robot to the center of the tile
+                direction = self.current_tile.parent_direction
+                distance = self.tile_size/2
+                if direction == Direction.UP :
+                    target_coord = self.robot_pos + np.array([0,distance])
+                elif direction == Direction.RIGHT :
+                    target_coord = self.robot_pos + np.array([distance,0])
+                elif direction == Direction.DOWN :
+                    target_coord = self.robot_pos + np.array([0,-distance])
+                elif direction == Direction.LEFT :
+                    target_coord = self.robot_pos + np.array([-distance,0])
+                target = Pose(Point(target_coord[0],target_coord[1],0),Quaternion())
+                self.target_publisher.publish(target)
+                self.state = State.MOVING_TO_CENTER
+                return
+            elif self.state == State.MAPPING :
+                # Get the closest points in each quadrant
+                quadrants = [[],[],[],[]] # [top_left,top_right,bottom_left,bottom_right]
+                walls = [False,False,False,False]
+                for marker in data.markers :
+                    points_in_quad = [False,False,False,False]
+                    for point in marker.points :
+                        # Change base from robot coords to general coords
+                        point = np.array([point.x,point.y])
+                        # Rotate point
+                        point = np.dot(np.array([[np.cos(self.robot_pos.pose.orientation.z),-np.sin(self.robot_pos.pose.orientation.z)],[np.sin(self.robot_pos.pose.orientation.z),np.cos(self.robot_pos.pose.orientation.z)]]),point)
+                        point = point + np.array([self.robot_pos.pose.position.x,self.robot_pos.pose.position.y])
+                        if point.x > self.robot_pos.pose.position.x :
+                            if point.y > self.robot_pos.pose.position.y :
+                                quadrants[0].append(point)
+                                points_in_quad[0] = True
+                            else :
+                                quadrants[3].append(point)
+                                points_in_quad[3] = True
+                        else :
+                            if point.y > self.robot_pos.pose.position.y :
+                                quadrants[1].append(point)
+                                points_in_quad[1] = True
+                            else :
+                                quadrants[2].append(point)
+                                points_in_quad[2] = True
+                    if points_in_quad[0] and points_in_quad[1] :
+                        walls[Direction.UP] = True
+                    if points_in_quad[1] and points_in_quad[3] :
+                        walls[Direction.RIGHT] = True
+                    if points_in_quad[2] and points_in_quad[3] :
+                        walls[Direction.DOWN] = True
+                    if points_in_quad[0] and points_in_quad[2] :
+                        walls[Direction.LEFT] = True
+
+                
+                # Sort points by distance to the robot
+                for quadrant in quadrants :
+                    quadrant.sort(key=lambda point: np.linalg.norm(np.array([self.robot_pos.pose.position.x,self.robot_pos.pose.position.y])-np.array([point.x,point.y])))
+                
+                # Get the closest point in each quadrant
+                closest_points = []
+                for quadrant in quadrants :
+                    closest_points.append(quadrant[0])
+                
+                # Delete points that are too far
+                for i in range(len(closest_points)):
+                    if np.linalg.norm(np.array([self.robot_pos.pose.position.x,self.robot_pos.pose.position.y])-np.array([closest_points[i].x,closest_points[i].y])) > self.tile_size/2 :
+                        closest_points[i] = None
+                
+                # Update the corners of the current tile
+                for i in range(len(closest_points)):
+                    if closest_points[i] is not None :
+                        self.current_tile.set_corner(i,closest_points[i])
+                self.current_tile.extrapolate_corners()
+
+                # If all corners are defined, create the children
+                if None not in self.current_tile.get_corners() :
+                    for direction in Direction :
+                        if not walls[direction]:
+                            self.current_tile.create_child(direction)
+                
+                # Decide to move :
+                # If tile is mapped, go to child
+                if None not in self.current_tile.get_corners() :
+                    for child in self.current_tile.children :
+                        if child is not None :
+                            if not child.visited :
+                                self.current_tile = child
+                                self.state = State.MOVING_TO_EDGE
+                                target_coord = self.current_tile.get_parent_edge()
+                                target = Pose(Point(target_coord[0],target_coord[1],0),Quaternion())
+                                self.target_publisher.publish(target)
+                                return
+                    # If all children are visited, go to parent
+                    self.current_tile = self.current_tile.parent
+                    self.state = State.MOVING_TO_CENTER
+                    target_coord = self.current_tile.get_center()
+                    target = Pose(Point(target_coord[0],target_coord[1],0),Quaternion())
+                    self.target_publisher.publish(target)
+                    return
+                else :
+                    pass
+
         
 
 
